3cb334c17628f636bfd31ac7549d26ba71d188ad
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-08-06 08:08:45 -0400

    cleaned up code

--- client/src/components/Playlist/PlaylistContainer.jsx
+++ client/src/components/Playlist/PlaylistContainer.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import CommentIcon from '@material-ui/icons/Comment';
 import {connect} from 'react-redux'
 import Button from '@material-ui/core/Button';
 import Playlist from './Playlist'
 import PlaylistPlayer from './PlaylistPlayer'
 import NavBar from '../NavBar'
-import {Switch, Route, withRouter, Redirect} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import header from '../../images/Orchid_header.jpg'
