2ba76dfc9753cc39be65b91cc8645db36f63df3f
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-08-16 18:21:21 -0400

    permanent app bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,26 +1,46 @@
 import React from 'react';
-import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
-import header from '../images/orchid_logo_square.jpg'
-import Box from "@material-ui/core/Box";
-import { ListItemText } from '@material-ui/core'
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import { MenuItem } from '@material-ui/core';
+import header from '../images/Orchid_header.jpg'
+import Box from '@material-ui/core/Box';
 
-const useStyles = makeStyles({
-  list: {
-    width: 250,
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
   },
-  fullList: {
-    width: 'auto',
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
   },
-  listItemText:{
-    fontSize:'1em',//Insert your required size
-  }
-});
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
 
 export default function NavBar(props) {
 const classes = useStyles();
@@ -47,24 +67,29 @@ const handleHomeClick = (e) => {
     props.history.push("/playlist")
   }
 
-const toggleDrawer = (anchor, open) => (event) => {
-  if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-    return;
-  }
 
-  setState({ ...state, [anchor]: open });
-};
 
-const list = (anchor) => (
-  <div
-    className={clsx(classes.list, {
-      [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-    })}
-    role="presentation"
-    onClick={toggleDrawer(anchor, false)}
-    onKeyDown={toggleDrawer(anchor, false)}
-  >
-    <br></br>
+
+  return (
+    <div className={classes.root}>
+    <CssBaseline />
+    {/* <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="h6" noWrap>
+          Permanent drawer
+        </Typography>
+      </Toolbar>
+    </AppBar> */}
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      anchor="left"
+    >
+      <div className={classes.toolbar} />
+      <br></br>
       <img src={header}
       id="logo"></img>
       <br></br>
@@ -76,47 +101,35 @@ const list = (anchor) => (
         <List>
         <ListItem button 
         onClick={handleHomeClick}
-        style={{ color: "#F9C6E8" }}>
-          <ListItemText>Home</ListItemText>
+        style={{ color: "#F9C6E8", backgroundColor: '#264ECD' }}>
+          <MenuItem>Home</MenuItem>
         </ListItem>
         <ListItem button 
         onClick={handleRegisterClick}
-        style={{ color: "#F9C6E8" }}>
-          <ListItemText>Register</ListItemText>
+        style={{ color: "#F9C6E8", backgroundColor: '#264ECD' }}>
+          <MenuItem>Register</MenuItem>
         </ListItem>
         <ListItem button 
         onClick={handlePeriodsClick}
-        style={{ color: "#F9C6E8" }}>
-          <ListItemText>Periods</ListItemText>
+        style={{ color: "#F9C6E8", backgroundColor: '#264ECD' }}>
+          <MenuItem>Periods</MenuItem>
         </ListItem>
         <ListItem button 
         onClick={handlePlaylistClick}
-        style={{ color: "#F9C6E8" }}>
-          <ListItemText>Playlist</ListItemText>
+        style={{ color: "#F9C6E8", backgroundColor: '#264ECD' }}>
+          <MenuItem>Playlist</MenuItem>
         </ListItem>
         <ListItem button 
         onClick={props.logout}
-        style={{ color: "#F9C6E8" }}>
-          <ListItemText>Logout</ListItemText>
+        style={{ color: "#F9C6E8", backgroundColor: '#264ECD' }}>
+          <MenuItem>Logout</MenuItem>
         </ListItem>
       
     </List>
     </Box>
+    </Drawer>
   </div>
-);
 
-
-  return (
-    <div>
-      {['navbar'].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
-    </div>
   )
     
 }
