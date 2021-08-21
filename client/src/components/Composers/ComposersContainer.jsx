99db16b6406e8a944fc5eba689d1b6f52474bf74
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-08-21 10:57:49 -0400

    composer background updated

--- client/src/components/Composers/ComposersContainer.jsx
+++ client/src/components/Composers/ComposersContainer.jsx
@@ -18,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
       marginRight: theme.spacing(2),
     },
     heroContent: {
-      backgroundColor: theme.palette.background.paper,
+      backgroundColor: theme.palette.tertiary.main,
       padding: theme.spacing(8, 0, 6),
     },
     heroButtons: {
@@ -54,9 +54,7 @@ function ComposersContainer(props) {
     
     return (
         <div>
-      <Card>
-        <img src={header}/>
-      </Card>
+
       <NavBar history={props.history}/>
         <React.Fragment>
         <CssBaseline />
