81d7176ac90b436f3d7c1d19c6b7308696977ede
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-09-18 12:51:00 -0400

    updated search bar color

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -33,7 +33,17 @@ const useStyles = makeStyles((theme) =>
   divider: {
     backgroundColor: theme.palette.secondary.main,
     borderWidth: "2px"
-  }
+  },
+  button: {
+    justifyContent: 'center',
+    background: 'linear-gradient(45deg, #ffc0cb 30%, #FF8E53 90%)',
+  borderRadius: 3,
+  border: 0,
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
 }));
 
 export default function NavBar(props) {
@@ -136,7 +146,7 @@ export default function NavBar(props) {
           <TextField noValidate autoComplete="off" 
                  onChange={onChange}
                 //  value={props.currentSearchBar}
-                 className={classes.root} 
+                className={classes.button}
                  id="outlined-basic" 
                  label="Search" 
                  variant="outlined"  
